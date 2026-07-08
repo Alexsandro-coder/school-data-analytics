7e982b08d485a6f54ad7570f3beb5e063e31c909
Alex Sandro Rodrigues Nobre Junior <alexsandro.oficial026@gmail.com>
2026-07-08 09:47:05 -0300

    Adiciona ordenação alfabética e filtro por bimestre

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pandas as pd
 df = pd.read_csv('alunos_fake_projeto.csv')
-# 1. Visualização original dos dados
+# Visualização original dos dados
 print(df.head())
 
-# 2. Definir a nota de corte para recuperação
+# Definir a nota de corte para recuperação
 nota_corte = 6.0
 
-# 3. Filtrar apenas os alunos com nota abaixo da média
-df_recuperacao = df[df["nota da fase"] < nota_corte]
+# Filtrar apenas os alunos com nota abaixo da média
+df_recuperacao = df[df['nota da fase'] < nota_corte]
 
-# 4. Mostrar quantos alunos ficaram de recuperação
-print(f"\nTotal de alunos em recuperação: {len(df_recuperacao)}")
-print("\n--- Lista de Alunos em Recuperação ---")
-print(df_recuperacao[["matricula", "nome", "materia", "nota da fase"]])
+# Organiza os nomes cadastrados
+df_recuperacao['nome'] = df_recuperacao['nome'].str.title()
+df_recuperacao = df_recuperacao.sort_values('nome')
+
+# Filtro por bimestre
+bimestre1 = df_recuperacao['fase de nota'] == '1 bimestre'
+bimestre2 = df_recuperacao['fase de nota'] == '2 bimestre'
+
+# Mostrar quantos alunos ficaram de recuperação
+print(f'\nTotal de alunos em recuperação: {len(df_recuperacao['nome'].unique())}')
+print('\n--- Lista de Alunos em Recuperação ---')
+print(df_recuperacao[['matricula', 'nome', 'materia','fase de nota', 'nota da fase']])
