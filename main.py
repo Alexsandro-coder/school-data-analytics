fc562f5f2d92911e8cdc5937b202c44ebbb4d214
Alex Sandro Rodrigues Nobre Junior <alexsandro.oficial026@gmail.com>
2026-07-08 11:42:03 -0300

    Refatora filtro de bimestre para ser dinâmico com unique()

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ print(df.head())
 nota_corte = 6.0
 
 # Filtrar apenas os alunos com nota abaixo da média
-df_recuperacao = df[df['nota da fase'] < nota_corte]
+df_recuperacao = df[df['nota da fase'] < nota_corte].copy()
 
 # Organiza os nomes cadastrados
 df_recuperacao['nome'] = df_recuperacao['nome'].str.title()
 df_recuperacao = df_recuperacao.sort_values('nome')
 
 # Filtro por bimestre
-bimestre1 = df_recuperacao['fase de nota'] == '1 bimestre'
-bimestre2 = df_recuperacao['fase de nota'] == '2 bimestre'
+bimestres = df_recuperacao['fase de nota'].unique()
+print('Bimestres disponíveis:')
+for i, b in enumerate(bimestres, 1):
+    print(f"[{i}] {b}")
+
+escolha = int(input('Qual bimestre deseja ver? '))
+bimestre_escolhido = bimestres[escolha - 1]
+
+# formatação do relatorio
+relatorio = df_recuperacao[df_recuperacao['fase de nota'] == bimestre_escolhido][['matricula', 'nome', 'materia','fase de nota', 'nota da fase']]
 
 # Mostrar quantos alunos ficaram de recuperação
-print(f'\nTotal de alunos em recuperação: {len(df_recuperacao['nome'].unique())}')
+print(f'\nTotal de alunos em recuperação: {len(relatorio['nome'].unique())}')
 print('\n--- Lista de Alunos em Recuperação ---')
-print(df_recuperacao[['matricula', 'nome', 'materia','fase de nota', 'nota da fase']])
+print(relatorio)
