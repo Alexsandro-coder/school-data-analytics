58ed57feeda45e6ce98e2c029f7bf935a085ca76
Alex Sandro <alexsandro.oficial026@gmail.com>
2026-07-13 14:40:55 -0300

    feat: adiciona seleção de bimestre para comparação

--- filtros.py
+++ filtros.py
@@ -9,6 +9,7 @@ def preparar_dados_recuperacao(df, nota_corte=6.0):
 def visualizar_bimestres_disponiveis(df_recuperacao):
     # Filtro por bimestre
     bimestres = df_recuperacao['fase de nota'].unique()
+
     print('Bimestres disponíveis:')
     for i, b in enumerate(bimestres, 1):
         print(f"[{i}] {b}")
@@ -33,3 +34,30 @@ def definir_grau_alerta(total):
         return 'ATENÇÃO'
     else:
         return 'CRÍTICO'
+
+def comparar_bimestres(bimestre_escolhido, df_recuperacao):
+    print(f'O {bimestre_escolhido} foi escolhido')
+
+    bimestres = df_recuperacao['fase de nota'].unique()
+
+    bimestre_disponiveis = [b for b in bimestres if b != bimestre_escolhido]
+
+    print('Bimestres disponíveis:')
+    for i, b in enumerate(bimestre_disponiveis, 1):
+        if b == bimestre_escolhido:
+            continue
+        print(f"[{i}] {b}")
+
+    bimestre_escolhido2 = ''
+    while True:
+        try:
+            escolha = int(input('Qual bimestre deseja comparar? '))
+            # Proteção contra o usuário
+            if 1 <= escolha <= len(bimestre_disponiveis):
+                bimestre_escolhido2 = bimestre_disponiveis[escolha - 1]
+                break
+            else:
+                print(f"Opção inválida. Digite um número entre 1 e {len(bimestre_disponiveis)}.")
+        except ValueError:
+            print('Digite apenas o numero correspondente')
+    return bimestre_escolhido2
